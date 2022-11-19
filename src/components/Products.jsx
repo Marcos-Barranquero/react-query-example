73cd95169c71db9ea1e928600f4912f4dee2c613
Marcos Barranquero <marcos.barranquero@uah.es>
2022-11-19 14:48:47 +0100

    using product petition

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,19 @@
+import { useQuery } from '@tanstack/react-query'
+import { getProducts } from '../api/productsAPI'
+
 function Products() {
-  return <div>Products List</div>
+  const { isLoading, data, isError, error } = useQuery({ queryKey: ['products'], queryFn: getProducts })
+
+  if (isLoading) return <div>Loading...</div>
+  else if (isError) return <div>Error: {error.message}</div>
+  else
+    return (
+      <div>
+        {data.map(product => (
+          <div key={product.id}>{product.name} - {product.price}</div>
+        ))}
+      </div>
+    )
 }
 
 export default Products
