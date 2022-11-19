9b01d6a7d57130e1df314415a560cfb0a97505fb
Marcos Barranquero <marcos.barranquero@uah.es>
2022-11-19 14:37:47 +0100

    backend added

--- doc/notes.md
+++ doc/notes.md
@@ -1,4 +1,15 @@
+# Video tutorial
+
+https://www.youtube.com/watch?v=srMBmtVeS00
+
 # Use of react query
+
 https://tanstack.com/query/v4/?from=reactQueryV3&original=https://react-query-v3.tanstack.com/
 
+# React Dev Tools
 
+Allows debugging in the webclient.
+
+# Json-server
+
+Reads a JSON an adds a REST API over it.
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+// Where the data is stored
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 )
