853bec8491fb577e194d0e9406377d6bfef44db5
Marcos Barranquero <marcos.barranquero@uah.es>
2022-11-19 16:56:38 +0100

    Displaying products properly

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,15 +2,22 @@ import { useQuery } from '@tanstack/react-query'
 import { getProducts } from '../api/productsAPI'
 
 function Products() {
-  const { isLoading, data, isError, error } = useQuery({ queryKey: ['products'], queryFn: getProducts })
+  const { isLoading, data: products, isError, error } = useQuery({ queryKey: ['products'], queryFn: getProducts })
 
   if (isLoading) return <div>Loading...</div>
   else if (isError) return <div>Error: {error.message}</div>
   else
     return (
       <div>
-        {data.map(product => (
-          <div key={product.id}>{product.name} - {product.price}</div>
+        {products.map(product => (
+          <div key={product.id}>
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+            <p>{product.price}</p>
+            <button>Delete</button>
+            <input type="checkbox" />
+            <label htmlFor="">In stock</label>
+          </div>
         ))}
       </div>
     )
